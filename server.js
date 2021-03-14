59df866666a33a4aa5a1c3bd5ba459da8115fe86
anhpn24 <anhphan2493@gmail.com>
2021-03-14 21:14:55 +0700

    Upload File

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+global.__basedir = __dirname;
+
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
@@ -53,6 +55,7 @@ app.get("/", (req, res) => {
 require("./routes/tutorial.routes")(app);
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
+require("./routes/upload.routes")(app);
 
 // set port, listen for requests
 app.listen(process.env.PORT || 3000);
